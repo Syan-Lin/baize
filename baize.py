5f1791bbc7b76034c47eb142e64c64b8a731b89b
Yuan. <lin.siyuan@foxmail.com>
2024-06-15 23:33:41 +0800

    bug: 暂时删去未实现的模型

--- baize.py
+++ baize.py
@@ -14,9 +14,9 @@ def get_llm(model_name: str, model_config: dict) -> BaseLLM:
     elif model_name in models['glm']['models']:
         from llm.glm import GLM
         llm = GLM(model_name, model_config)
-    elif model_name in models['openai']['models']:
-        from llm.openai import GPT
-        llm = GPT(model_name, model_config)
+    else:
+        rprint(f'[red]不支持模型 {model_name}[/red]')
+        sys.exit()
 
     return llm
 
