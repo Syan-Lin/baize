50afadf62a5d410f9b2f44f0f5c2af8e56d3b3b1
Yuan. <lin.siyuan@foxmail.com>
2024-06-25 00:58:41 +0800

    feat: 新增 workflow 初始化图

---
+++ workflow/graph.py
@@ -0,0 +1,101 @@
+import sys
+import json
+from rich import print as rprint
+from workflow.node import (
+    Node,
+    OutputNode,
+    make_input_node,
+    make_llm_node,
+    make_output_node,
+    make_script_node
+)
+
+
+def init_graph(workflow_config_path: str, debug: bool):
+    with open(workflow_config_path, 'r', encoding='utf-8') as f:
+        workflow_config = json.load(f)
+
+    graph = Graph()
+    nodes = {}
+    for name, config in workflow_config.items():
+        if config['type'] == 'input':
+            node = make_input_node(name, config)
+        elif config['type'] == 'llm':
+            node = make_llm_node(name, config)
+        elif config['type'] == 'script':
+            node = make_script_node(name, config)
+        elif config['type'] == 'output':
+            node = make_output_node(name, config)
+        else:
+            rprint(f'节点 [red]{name}[/red] 类型错误，不存在类型 {config['type']}')
+            sys.exit()
+        if debug:
+            node.enable_debug()
+        graph.add_node(node)
+        nodes[name] = node
+
+    for name, config in workflow_config.items():
+        if 'input' not in config:
+            continue
+        for input_node in config['input']:
+            if input_node not in nodes:
+                rprint(f'节点 [red]{name}[/red] 的输入节点 [red]{input_node}[/red] 不存在！')
+                sys.exit()
+            nodes[name].add_input(nodes[input_node])
+
+    graph.check_loop()
+
+    return graph
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.output_node = None
+
+
+    def add_node(self, node: Node):
+        if node in self.nodes:
+            rprint(f'节点 [red]{node.name}[/red] 已经存在！')
+            sys.exit()
+        if isinstance(node, OutputNode):
+            if self.output_node is not None:
+               rprint(f'只能存在一个输出节点，节点 [red]{node.name}[/red] 产生重复！')
+               sys.exit()
+            self.output_node = node
+
+        self.nodes.append(node)
+
+
+    def check_loop(self):
+        states = {}
+        for node in self.nodes:
+            states[node] = 0
+        loop_node = None
+
+        def dfs(node: Node):
+            nonlocal loop_node
+            if loop_node is not None or states[node] == 2:
+                return
+            if states[node] == 1:
+                loop_node = node
+                return
+            states[node] = 1
+            for next_node in node.input:
+                dfs(next_node)
+            states[node] = 2
+
+        for node in self.nodes:
+            if states[node] == 0:
+                dfs(node)
+
+        if loop_node is not None:
+            rprint(f'Workflow 存在环，出现在节点 [red]{loop_node.name}[/red]')
+            sys.exit()
+
+
+    def run(self):
+        if self.output_node is None:
+            rprint('[red]Workflow 没有输出节点[/red]')
+            sys.exit()
+        return self.output_node.output()
