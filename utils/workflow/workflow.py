8171cbe6cd55569e56cf986675f2b5e9d0a22aff
linsiyuan <linsiyuan03@meituan.com>
2024-06-26 17:31:35 +0800

    fix: 修复配置文件读取为转为 dict 的错误

--- utils/workflow/workflow.py
+++ utils/workflow/workflow.py
@@ -7,5 +7,6 @@ from utils.resource import get_resource, ResourceType
 
 def workflow_main(args: Namespace):
     workflow_config = get_resource(ResourceType.workflow, args.workflow[0])
+    workflow_config = json.loads(workflow_config)
     workflow = init_graph(workflow_config, args.log)
     workflow.run()
