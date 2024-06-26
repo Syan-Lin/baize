9e04e2e694aabf9d42490715cab5af2540993c17
Yuan. <lin.siyuan@foxmail.com>
2024-06-27 01:10:14 +0800

    feat: 优化 Tool 模式交互

--- utils/tool/tool.py
+++ utils/tool/tool.py
@@ -8,13 +8,20 @@ from llm.base_llm import BaseLLM
 from utils.render import print_markdown
 from utils.resource import get_resource, get_resource_path, ResourceType
 
+SYSTEM_PROMPT = '''
+# OBJECTIVE
+你是一个 Agent 助手，你会根据用户的请求构造调用函数的参数，并调用函数，最后基于函数的返回值给出相应的响应。
+
+请你深呼吸，保持专注，按照以下要求逐点检查：
+1. 你需要逐一检查函数调用的所有必须参数，用户是否提供了足够的信息来构造函数调用所需的所有参数；如果没有，你需要询问用户相对应的参数信息，并等待用户回复。
+2. 不要捏造参数字段，如果用户没有提供这些参数，必须询问用户并等待用户回复。
+'''
+
 CODE_TEMPLATE = '''
 {custom_code}
 result = {function}({params})
 print(result)
 '''
-
-MAX_RETRY = 3
 
 
 def tool_main(args: Namespace, llm: BaseLLM):
@@ -33,20 +40,22 @@ def tool_main(args: Namespace, llm: BaseLLM):
 
     # function call
     messages = []
-    retry = 0
+    messages.append({
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    })
 
     while True:
         print('> ', end='')
         prompt = input()
+        if prompt.lower() == 'exit' or prompt.lower() == 'q':
+            sys.exit()
         user_message = {'role': 'user', 'content': prompt}
         messages.append(user_message)
 
-        while retry < MAX_RETRY:
-            response, function, params, tool_message = llm.call_tool(tools, messages)
-            make_function_call = function is not None and params is not None and tool_message is not None
-            if not make_function_call:
-                break
-
+        response, function, params, tool_message = llm.call_tool(tools, messages)
+        make_function_call = function is not None and params is not None and tool_message is not None
+        if make_function_call:
             # 构造调用
             messages.append(tool_message)
             params = json.loads(params)
@@ -59,17 +68,13 @@ def tool_main(args: Namespace, llm: BaseLLM):
             result = subprocess.run([python, "-c", exec_code], capture_output=True, text=True)
 
             if args.log:
+                from utils.context import print_messages
+                print_messages(messages)
                 rprint(f'\ncode:[red]{exec_code}[/red]')
                 rprint(f'output:\n[yellow]{result.stdout}[/yellow]')
 
             if result.stderr:
-                messages.append({
-                    "role": "tool",
-                    "content": f'调用出现错误，请你重新调用：\n{result.stderr}',
-                    "tool_call_id": tool_message['tool_calls'][0]['id']
-                })
-                retry += 1
-                continue
+                script_output = result.stderr
             else:
                 script_output = result.stdout
 
@@ -80,11 +85,18 @@ def tool_main(args: Namespace, llm: BaseLLM):
             })
 
             result = llm.message(messages)
-            rprint(f'[green]{result}[/green]')
-            sys.exit()
+            buffer = ''
 
-        if retry == MAX_RETRY:
-            rprint(f'[red]调用工具失败，请重新尝试[/red]')
-            sys.exit()
-        rprint(f'[green]{response}[/green]')
-        messages.append({'role': 'assistant', 'content': response})
+            response = llm.stream_message(messages)
+            if args.markdown:
+                from utils.render import print_markdown_stream
+                buffer = print_markdown_stream(response)
+            else:
+                for block in response:
+                    buffer += block
+                    sys.stdout.write(block)
+                    sys.stdout.flush()
+            sys.stdout.write('\n')
+        else:
+            rprint(f'[green]{response}[/green]')
+            messages.append({'role': 'assistant', 'content': response})
