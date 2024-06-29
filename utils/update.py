da0f53c86ea2b64341e6d56a73ff90ac759b7f88
Yuan. <lin.siyuan@foxmail.com>
2024-06-29 12:50:44 +0800

    fix: 更新无法替换原本可执行程序

--- utils/update.py
+++ utils/update.py
@@ -14,13 +14,19 @@ from rich.progress import (
 GIT_URL = 'https://api.github.com/repos/Syan-Lin/baize/releases/latest'
 
 
+def get_path(file: str):
+    user_home = os.path.expanduser('~')
+    root_path = os.path.join(user_home, 'baize', file)
+    return root_path
+
+
 def get_latest_release():
     response = requests.get(GIT_URL)
     response.raise_for_status()
     return response.json()
 
 
-def download_asset(url: str, filename: str):
+def download_asset(url: str, file_path: str):
     response = requests.get(url, stream=True)
     response.raise_for_status()
 
@@ -31,31 +37,32 @@ def download_asset(url: str, filename: str):
         TimeRemainingColumn(),
     )
 
-    with open(filename, 'wb') as f:
+    with open(file_path, 'wb') as f:
         block_size = 1024
         total_size = int(response.headers.get('content-length', 0))
 
         with progress:
-            task = progress.add_task(f'"{filename}" 下载中...', total=total_size)
+            task = progress.add_task(f'"{file_path}" 下载中...', total=total_size)
             while not progress.finished:
                 progress.update(task, advance=block_size)
                 buffer = response.raw.read(block_size)
                 f.write(buffer)
 
 
-def unpack_zip(filename: str):
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
-        zip_ref.extractall('.')
+def unpack_zip(file_path: str):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(os.path.join(os.path.expanduser('~'), 'baize'))
 
     zip_ref.close()
-    os.remove(filename)
+    os.remove(file_path)
 
 
 def download_file(assets: list, file_name: str) -> bool:
     for asset in assets:
         if asset['name'] == file_name:
-            download_asset(asset['browser_download_url'], file_name)
-            unpack_zip(file_name)
+            file_path = get_path(file_name)
+            download_asset(asset['browser_download_url'], file_path)
+            unpack_zip(file_path)
             return True
     return False
 
@@ -90,10 +97,19 @@ def update():
     if choice == 'n':
         return
 
-    assets = release_info['assets']
-    if not download_file(assets, 'resource.zip'):
-        rprint(f'[red]错误: 下载资源失败！[/red]')
-        return
-    if not download_exec(assets):
-        rprint(f'[red]错误: 下载可执行文件失败！[/red]')
-        return
+    baize_path = get_path('baize')
+    backup_path = get_path('baize_bak')
+
+    try:
+        os.rename(baize_path, backup_path)
+        assets = release_info['assets']
+        if not download_file(assets, 'resource.zip'):
+            rprint(f'[red]错误: 下载资源失败！[/red]')
+            raise Exception
+        if not download_exec(assets):
+            rprint(f'[red]错误: 下载可执行文件失败！[/red]')
+            raise Exception
+    except:
+        os.rename(backup_path, baize_path)
+    os.remove(backup_path)
+    os.chmod(baize_path, 0o755)
