a30f8c2d2a0428cd6f8c3154a4877f2a2ad28685
linsiyuan <linsiyuan03@meituan.com>
2024-07-09 18:42:22 +0800

    res: 优化 interpreter 配置

--- tool/interpreter/tool.py
+++ tool/interpreter/tool.py
@@ -1,7 +1,9 @@
 import subprocess
-def exec_code(code: str) -> dict:
+def exec_code(code: str):
+    # 执行 pyhon 代码，返回其中 print 函数输出的结果
+    # code: 所要执行的 python 代码，只有执行 print 函数才会得到相应的输出
     result = subprocess.run(['python', '-c', code], capture_output=True, text=True)
-    return {
-        'stdout': result.stdout,
-        'stderr': result.stderr
-    }
+    if result.stderr:
+        return result.stderr
+    if result.stdout:
+        return result.stdout
