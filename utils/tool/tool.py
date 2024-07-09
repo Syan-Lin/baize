963a9e40606e8d22446c12b6d40715799cc945d9
linsiyuan <linsiyuan03@meituan.com>
2024-07-09 18:41:41 +0800

    feat: 优化 tool 模式下的交互和信息展示

--- utils/tool/tool.py
+++ utils/tool/tool.py
@@ -8,18 +8,8 @@ from argparse import Namespace
 from llm.base_llm import BaseLLM
 from utils.render import print_code
 from utils.resource import get_resource, print_resource_table, get_resource_path, ResourceType
+from utils.setup import input_param
 
-SYSTEM_PROMPT = '''
-# OBJECTIVE
-你是一个 Agent 助手，你会根据用户的请求构造调用函数的参数，并调用函数，最后基于函数的返回值给出相应的响应。
-
-如果用户没有提供足够的信息来调用函数，请继续提问以确保收集到了足够的信息。
-在调用函数之前，你必须总结用户的描述并向用户提供总结，询问他们是否需要进行任何修改。
-
-请你深呼吸，保持专注，按照以下要求逐点检查：
-1. 你需要逐一检查函数调用的所有必须参数，用户是否提供了足够的信息来构造函数调用所需的所有参数；如果没有，你需要询问用户相对应的参数信息，并等待用户回复。
-2. 不要捏造参数字段，如果用户没有提供这些参数，必须询问用户并等待用户回复。
-'''
 
 CODE_TEMPLATE = '''
 {custom_code}
@@ -33,21 +23,26 @@ def create_tool():
     rprint('准备开始创建工具，代码会自动从剪贴板获取，请确保 [green]剪贴板中已经复制[/green] 了代码，程序在输入任意键后开始...', end='')
     input()
     tool_code = pyperclip.paste()
-    rprint('请输入 [green]工具名称[/green]: ', end='')
-    tool_name = input()
-    rprint('请用 [green]一句话[/green] 描述工具: ', end='')
-    tool_describe = input()
-    rprint('请输入 [green]作者名[/green]: ', end='')
-    author = input()
+    rprint('系统 Prompt 会自动从剪贴板获取，请确保 [green]剪贴板中已经复制[/green] 了系统 Prompt，程序在输入任意键后开始...', end='')
+    input()
+    sys_prompt = pyperclip.paste()
+    tool_name = input_param('工具名称')
+    tool_describe = input_param('工具描述', skip=True)
+    author = input_param('作者', skip=True)
+    python_path = input_param('Python 路径', skip=True)
+    if python_path is None:
+        python_path = 'python'
     date = datetime.now().strftime('%Y-%m-%d')
 
+    rprint('\n[red]系统 Prompt[/red]:\n')
+    print(sys_prompt)
     rprint('\n[blue]工具代码[/blue]:\n')
     print_code(tool_code, 'python')
     from utils.tool.signature import get_function_signatures
     try:
         signatures = get_function_signatures(tool_code)
     except Exception as e:
-        rprint('[red]错误: 代码解析失败，请检查是否有语法错误！[/red]')
+        rprint(f'[red]错误: 代码解析失败，请检查是否有语法错误！[/red]\n{e}')
         sys.exit()
     rprint('\n[blue]工具描述[/blue]:\n', json.dumps(signatures, ensure_ascii=False, indent=4))
 
@@ -82,8 +77,14 @@ def create_tool():
             }, f, ensure_ascii=False)
         with open(os.path.join(new_tool_path, 'tool.py'), 'w', encoding='utf-8') as f:
             f.write(tool_code)
+        with open(os.path.join(new_tool_path, 'sys.md'), 'w', encoding='utf-8') as f:
+            f.write(sys_prompt)
         with open(os.path.join(new_tool_path,'tool.json'), 'w', encoding='utf-8') as f:
-            json.dump(signatures, f, ensure_ascii=False)
+            tool_config = {
+                'python': python_path,
+                'tools': signatures
+            }
+            json.dump(tool_config, f, ensure_ascii=False)
         info = f'工具创建成功！保存在目录 {new_tool_path}'
         rprint('[green]' + info + '[/green]')
 
@@ -95,6 +96,7 @@ def tool_main(args: Namespace, llm: BaseLLM):
     tools = config['tools']
     python = config['python']
     script_path = os.path.join(get_resource_path(ResourceType.tool), args.tool[0], 'tool.py')
+    sys_prompt = os.path.join(get_resource_path(ResourceType.tool), args.tool[0], 'sys.md')
 
     try:
         with open(script_path, 'r', encoding='utf-8') as f:
@@ -103,16 +105,13 @@ def tool_main(args: Namespace, llm: BaseLLM):
         raise ValueError(f'读取脚本文件 {script_path} 失败: {e}')
 
     # function call
-    messages = []
-    messages.append({
-        "role": "system",
-        "content": SYSTEM_PROMPT
-    })
+    messages = [{'role': 'system', 'content': sys_prompt}]
 
+    rprint('[green]（工具模式中输入 `/q` 退出）[/green]', end='')
     while True:
         print('> ', end='')
         prompt = input()
-        if prompt.lower() == 'exit' or prompt.lower() == 'q':
+        if prompt.lower() == '/q':
             sys.exit()
         user_message = {'role': 'user', 'content': prompt}
         messages.append(user_message)
@@ -130,17 +129,18 @@ def tool_main(args: Namespace, llm: BaseLLM):
                 params_str += f'{param}="""{value}""",'
             params_str = params_str.rstrip(',')
 
+            from utils.render import print_markdown
+            print_markdown(f'```python\n{'{function}({params})'.format(function=function, params=params_str)}\n```')
+
             rprint(f'[blue]正在调用[/blue] [green]`{function}`[/green]')
             exec_code = CODE_TEMPLATE.format(custom_code=custom_code, function=function, params=params_str)
             result = subprocess.run([python, "-c", exec_code], capture_output=True, text=True)
 
             if args.log:
-                from utils.render import print_code, print_markdown
-                print_markdown(f'```python\n{'{function}({params})'.format(function=function, params=params_str)}\n```')
+                if result.stdout:
+                    print_markdown(f'输出: {result.stdout}')
                 if result.stderr:
-                    print_markdown(f'{result.stderr}')
-                else:
-                    print_markdown(f'{result.stdout}')
+                    print_markdown(f'错误: {result.stderr}')
 
             if result.stderr:
                 script_output = result.stderr
