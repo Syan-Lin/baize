4502cde2b6d3db148d726f3e6ed04a9745f20b54
linsiyuan <linsiyuan03@meituan.com>
2024-07-26 17:26:05 +0800

    feat: 优化 log 信息

--- utils/log.py
+++ utils/log.py
@@ -12,8 +12,12 @@ logging.basicConfig(filename=log_file, level=logging.INFO,
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logging.info(f"Calling function {func.__name__} with args: {args} and kwargs: {kwargs}")
+        if len(args) == 0:
+            logging.info(f"Calling function: `{func.__name__}`")
+        else:
+            logging.info(f"Calling function `{func.__name__}` with args: {args}")
         result = func(*args, **kwargs)
-        logging.info(f"Function {func.__name__} returned {result}")
+        if result:
+            logging.info(f"Function `{func.__name__}` returned: {result}")
         return result
     return wrapper
